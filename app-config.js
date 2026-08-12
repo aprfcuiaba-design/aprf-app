1c56247ea0464dda9beaba7a06bba9b1598422d5
aprfcuiaba-design <aprfcuiaba@gmail.com>
2026-08-12 14:03:41 -0400

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -1,2 +1,2 @@
 // Endereço atual do Sistema APRF
-const APRF_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzg8UIBO4A_Pz4od2UIAJH34K8jsCtAYfxbz6PjwayuTTsY5JWp3fbiCyCpl5GtUuGFMaQ/exec";
+const APRF_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwEsgDqjZcZ0yQNBumOVWAkAjuEerTMMKpZDuThI-FN6s3ibawBCO5cZdzVW0q8KMy6/exec";
